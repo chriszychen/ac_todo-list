483738520f2d1cb63b35fbe797ccebf016b7fdfc
Chris Chen <ok3201123@gmail.com>
2021-05-02 22:49:02 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -23,14 +23,35 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 // set routes
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(err => console.error(err))
+
 })
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  console.log(req)
+  // 先產生實例，再存入資料庫
+  // const todo = new Todo({ name })
+  // return todo.save()
+  //   .then(() => res.redirect('/'))
+  //   .catch(err => console.log(err))
+
+  return Todo.create({ name })
+    .then(res.redirect('/'))
+    .catch(err => console.log(err))
+
+})
 
 // start and listen the server
 
